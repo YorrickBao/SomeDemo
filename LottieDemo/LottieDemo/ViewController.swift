5b5cec169c4ee2a71344a1d8e6fc8df3f73f638d
YorrickBao <yorrickpaul@126.com>
2017-04-24 14:27:56 +0800

    add lottieDemo

---
+++ LottieDemo/LottieDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LottieDemo
+//
+//  Created by YorrickBao on 2017/4/24.
+//  Copyright © 2017年 YorrickBao. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
